19a2a8e5eb7afeb3009e9820064fe30b3d9f6469
jayashrisathe <70744196+jayashrisathe@users.noreply.github.com>
2021-07-05 12:18:48 +0530

    Update sort_json_data.py

--- sort_json_data.py
+++ sort_json_data.py
@@ -3,26 +3,19 @@ import json
 
 def sort_json(data):
     output_data = []
-    parent_ids = []
-    for category in data:
-        if not category["parent_id"]:
-            parent_ids.insert(0, category["parent_id"])
-        else:
-            parent_ids.append(category["parent_id"])
-    for parent_id in parent_ids:
+    category_ids = [category["id"] for category in data if not category["parent_id"]]
+    output_data = [category for category in data if not category["parent_id"]]
+    while len(category_ids) < len(data):
         for category in data:
-            if category["parent_id"] == parent_id:
+            if category["parent_id"] in category_ids and category["id"] not in category_ids:
+                category_ids.append(category["id"])
                 output_data.append(category)
-    print(output_data)
+    return output_data
 
-
-"Data from class variable"
-input_data='[{"name": "Accessories","id": 1,"parent_id": 20},{"name": "Watches","id": 57,"parent_id": 1},{"name": "Men","id": 20,"parent_id": null}]'
-data = json.loads(input_data)  # Uncomment it if you want to take data from constant/class variable.
 
 "Data from file"
 f = open("input_data.json", )
 data = json.load(f)
 f.close()
 
-sort_json(data)
+result = sort_json(data)
